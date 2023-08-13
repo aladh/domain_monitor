815413a4c445e8f681ccead404ae890e918a0b20
Alishan Ladhani <git@alad.ca>
2023-08-13 17:58:03 -0400

    Handle RDAP services responsible for multiple tlds

--- src/main/kotlin/RdapRegistry.kt
+++ src/main/kotlin/RdapRegistry.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.json.JsonNames
 @Serializable
 private data class ServiceList(private val services: List<List<List<String>>>) {
     fun serviceForTLD(tld: String): String? =
-        services.find { it[0][0] == tld }?.get(1)?.get(0)
+        services.find { it[0].contains(tld) }?.get(1)?.get(0)
 }
 
 private const val BOOTSTRAP_URL = "https://data.iana.org/rdap/dns.json"
