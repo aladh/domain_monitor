6962e351df256dc0cc00f819e22119740b92cf83
Alishan Ladhani <git@alad.ca>
2023-11-09 16:13:53 -0500

    Update Kotlin to 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,36 @@
 plugins {
-    application
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+  application
+  kotlin("jvm") version "1.9.20"
+  kotlin("plugin.serialization") version "1.9.20"
 }
 
 group = "ca.alad"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 val ktor_version: String by project
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0-RC")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+  testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0-RC")
+  implementation("io.ktor:ktor-client-core:$ktor_version")
+  implementation("io.ktor:ktor-client-cio:$ktor_version")
+  implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(8)
+  jvmToolchain(17)
 }
 
 application {
-    mainClass.set("MainKt")
+  mainClass.set("MainKt")
 }
